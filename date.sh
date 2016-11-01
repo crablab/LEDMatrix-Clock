6b74068fe899ff29d4dc33db91f22ff331c5e98e
Hugh Wells <admin@crablab.co.uk>
2016-11-01 12:32:58 +0000

    Added files

---
+++ date.sh
@@ -0,0 +1,2 @@
+(while :; do date +"%D" ; sleep 0.2 ; done) | sudo ./text-example -f fonts/8x13B.bdf -y8 -x32 -c4 -C0,0,255
+
---
+++ time.sh
@@ -0,0 +1,2 @@
+(while :; do date +"%r" ; sleep 0.5 ; done) | sudo ./text-example -f fonts/7x14.bdf -y8 -x10 -c4 -C100,100,100 
+
